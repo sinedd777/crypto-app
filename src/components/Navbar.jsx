ed8856096f41e164dc1a312b77fadb340cce16e0
sinedd777 <gaurang.work20@gmail.com>
2022-07-29 22:59:49 +0530

    Layout Ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import icon from '../images/cryptocurrency.png'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size='large' />
+            <Typography.Title level={2} className='logo'>
+                <Link to='/'>Cryptoverse</Link>
+            </Typography.Title>
+        </div>
+        <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined></HomeOutlined>}>
+                <Link to='/'>Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined></FundOutlined>}>
+                <Link to='/cryptocurrencies'>Cryptocurriencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined></MoneyCollectOutlined>}>
+                <Link to='/exchanges'>Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined></BulbOutlined>}>
+                <Link to='/news'>News</Link>
+            </Menu.Item>
+        </Menu>
+
+    </div>
+  )
+}
+
+export default Navbar
